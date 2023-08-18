a0439ba09ef5692f3919424f9b4492c3fd71353e
Imper1um <github@imper1umdesigns.com>
2023-08-17 23:22:42 -0400

    Fixing sw5e
    Adding in a sw5e override to create5eAbilities that fixes the reference issue.

--- src/lmrtfy_RollProvider_sw5e.js
+++ src/lmrtfy_RollProvider_sw5e.js
@@ -14,4 +14,20 @@ class lmrtfy_RollProvider_sw5e extends lmrtfy_RollProvider_dnd5e {
 	normalRollEvent() {
 		return new lmrtfy_RollEvent(true, false, false);
 	}
+	
+	create5eAbilities() {
+		let abbr = {};
+        
+        for (let key in CONFIG.DND5E.abilities) { 
+            let abb = game.i18n.localize(key);
+            let upperFirstLetter = abb.charAt(0).toUpperCase() + abb.slice(1);
+			var localizedAbility = `SW5E.Ability${upperFirstLetter}`;
+			abbr[abb] = localizedAbility;
+            //abbr[`${abb}`] = `SW5E.Ability${upperFirstLetter}`;
+        }
+		
+		
+
+        return abbr;
+	}
 }
